c592608669548696e1e56698645455b38cd743ae
Shane <mhurtado1@berklee.edu>
2022-12-12 09:38:58 -0600

    func-math

---
+++ main.js
@@ -0,0 +1,15 @@
+const addStuff = (num1, num2 ) => {
+    const sum = num1 + num2 
+    return sum
+}
+
+let addition = addStuff(20, 14)
+console.log(addition)
+
+const multiplyStuff = (num1) => {
+    const sum = num1 * 2 
+    return sum
+}
+
+let multiplication = multiplyStuff(34)
+console.log(multiplication)
